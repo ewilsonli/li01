ed017fe858ac853de9993244966f5aa995200c10
liwei <66592519@qq.com>
2020-06-08 11:35:11 +0800

    222

--- mytest/src/controller/CourseController.java
+++ mytest/src/controller/CourseController.java
@@ -9,13 +9,13 @@ public class CourseController {
    private CourseService courseService;
    private TeacherService teacherService;
    
-   public String to1() {
+   public String tocreateCourse() {
     return null;
    }
    
    /** @param course 
     * @param teacherids */
-   public String docreateCourse(Course course, int[] teacherids) {
+   public String docreateCourse2(Course course, int[] teacherids) {
     return null;
    }
 
