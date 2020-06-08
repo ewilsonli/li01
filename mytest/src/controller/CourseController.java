877eb6750a6977ed8429cb8f1f5d5553d0ec7ac1
liwei <66592519@qq.com>
2020-06-08 15:26:42 +0800

    dqqdeqd

--- mytest/src/controller/CourseController.java
+++ mytest/src/controller/CourseController.java
@@ -11,10 +11,7 @@ public class CourseController {
    
    public String tocreateCourse() {
     return null;
-    //   333333
-   // i am 333
-    //i  am  222
-   }//i am 111
+   }
    
   /** @param course 
     * @param teacherids */
